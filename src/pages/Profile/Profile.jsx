b0e77f9b7bc0ff1bf8bf2d44130c61704ff61771
Valentina <invalofu.15@gmail.com>
2023-11-30 15:09:07 +0100

    profile done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,19 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import MenuList from "../../components/MenuList/MenuList"
 
-import { Container, Row, Col, Image } from "react-bootstrap"
+import { Container, Row, Col, Image, Button, Offcanvas } from "react-bootstrap"
 import { AuthContext } from "../../contexts/auth.contexts"
+import EditProfile from "../../components/EditProfile/EditProfile"
+
 
 const ProfilePage = () => {
 
     const { loggedUser } = useContext(AuthContext)
+
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
 
     return (
 
@@ -14,19 +21,30 @@ const ProfilePage = () => {
 
             <Row>
 
-                <Col xs={6} md={4}>
-
+                <Col >
                     <Image src={loggedUser.avatar} roundedCircle />
-
-                    {/* <Link to={'/menulist'} className='nav-link'>My menus</Link> */}
-
                 </Col>
 
                 <Col>
+                    <h1>{loggedUser.username}</h1>
 
-                    <h1> Este es tu perfil,{loggedUser.username}</h1>
+                    <Button variant="primary" onClick={handleShow} className="me-2">
+                        Editar
+                    </Button>
 
-                    <h2> Datos de: {loggedUser.username}</h2>
+                    <Offcanvas show={show} onHide={handleClose} >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title>{loggedUser.username}</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            {/* <Image src={loggedUser.avatar} style={{ width: '100px' }} roundedCircle />
+
+                            <p>{loggedUser.username}</p>
+                            <p>{loggedUser.email}</p> */}
+                            <EditProfile />
+                        </Offcanvas.Body>
+
+                    </Offcanvas>
 
                 </Col>
 
