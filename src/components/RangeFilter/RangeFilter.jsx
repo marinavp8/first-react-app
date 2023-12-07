ead0c8746871bd0c75b3c2362efd16ff84566822
Marina Viejo <100945399+marinavp8@users.noreply.github.com>
2023-12-07 09:15:48 +0100

    multiple filter

---
+++ src/components/RangeFilter/RangeFilter.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+
+function RangeForm() {
+    const [calories, setCalories] = useState(300)
+    const [time, setTime] = useState(20)
+
+    return (
+        <form>
+            <div>
+                <label>
+                    Calories: {calories}
+                    <input
+                        type="range"
+                        min="0"
+                        max="1000"
+                        value={calories}
+                        onChange={(e) => setCalories(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Time (min): {time}
+                    <input
+                        type="range"
+                        min="0"
+                        max="120"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                    />
+                </label>
+            </div>
+        </form>
+    )
+}
+
+export default RangeForm
