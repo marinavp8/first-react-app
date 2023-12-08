9c1a09571e45eeb39d86f2346e851acb1e0a768a
Lucia Sanz <lu.sanz.galvez@gmail.com>
2023-12-08 09:47:12 +0100

    lastCommit

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,13 +4,13 @@ import listServices from '../../services/list.services'
 import usersServices from '../../services/users.services'
 import { AuthContext } from '../../contexts/auth.contexts'
 import { IoTrash } from "react-icons/io5"
+
 const List = () => {
 
   const { loggedUser } = useContext(AuthContext)
 
   const [ingredients, setIngredients] = useState([])
   const [newIngredient, setNewIngredient] = useState('')
-
   const [dataList, setDataList] = useState([])
 
   const handleInputChange = (e) => {
@@ -28,26 +28,18 @@ const List = () => {
     }
 
   }
-  const handleDeleteIngredient = (i) => {
+  const handleDeleteIngredient = (elm, i) => {
     const newList = [...ingredients]
     newList.splice(i, 1)
     setIngredients(newList)
 
     listServices
-      .deleteIngredient(newIngredient)
-      .then(() => console.log(newIngredient))
-      .catch(err => console.log(err))
-
-  }
-  const handleDeleteIngredient2 = (elm) => {
-
-    listServices
       .deleteIngredient(elm)
       .then(() => chargeList())
       .catch(err => console.log(err))
+
   }
-
-
+ 
   const chargeList = () => {
 
 
@@ -66,30 +58,11 @@ const List = () => {
 
 
   return (
-<<<<<<< HEAD
 
-    <>
+     <>
 
       <InputGroup className="mb-3" type="text" value={newIngredient} onChange={handleInputChange} >
-=======
-
-    <>
-      {
-
-        dataList.map((elm, i) => {
-          return (
-            <>
-              <p>{elm}</p>
-              <Button style={{ backgroundColor: 'rgb(58, 125, 19)', border: 'none' }} id="button-addon1" onClick={() => handleDeleteIngredient2(elm)}>Delete</Button>
-            </>
-          )
-        })
-      }
-      <InputGroup className="mb-3" type="text" value={newIngredient} onChange={handleInputChange} >
-        <Button style={{ backgroundColor: 'rgb(58, 125, 19)', border: 'none' }} id="button-addon1" onClick={handleAddIngredient}>
-          Button
-        </Button>
->>>>>>> cf1d9d8872d676ed2d6122a55cf8a10e45670645
+       
         <Form.Control
           aria-label="Example text with button addon"
           aria-describedby="basic-addon2"
@@ -98,45 +71,38 @@ const List = () => {
           Add to list
         </Button>
       </InputGroup>
+
       <ul>
-        {ingredients.map((ingredient, i) => (
+        {ingredients.map((ingredient, i) => 
           <>
             <Form.Check type="radio" aria-label="radio 1" key={i} > </Form.Check>
             {ingredient}
-<<<<<<< HEAD
-=======
-            <Button style={{ backgroundColor: 'rgb(58, 125, 19)', border: 'none' }} id="button-addon1" onClick={() => handleDeleteIngredient(i)}>Borrar</Button>
->>>>>>> cf1d9d8872d676ed2d6122a55cf8a10e45670645
           </>
-        ))}
+           )}
+       
       </ul>
-
-      {
-
-        dataList.map((elm, i) => {
+        {dataList.map((elm, i) => {
           return (
             <>
-              <Row>
-
-                <Col key={i} style={{ display: 'flex', alignItems: 'center' }}>
-                  <p>-   {elm}</p>
-                </Col>
-
-                <Col>
-                  <Button variant="outline-danger" id="button-addon1" onClick={() => handleDeleteIngredient(elm)}>
-                  <IoTrash />
-                    </Button>
-                </Col>
-
-              </Row>
-            </>
+            <Row>
+            <Col key={i} style={{ display: 'flex', alignItems: 'center' }}>
+              <p>-   {elm}</p>
+            </Col>
+            <Col>
+              <Button variant="outline-danger" id="button-addon1" onClick={() => handleDeleteIngredient(elm)}>
+              <IoTrash />
+                </Button>
+            </Col>
+          </Row>
+          </>
           )
-        })
+        }
+        )
       }
-
-    </>
+        </>  
   )
 }
+
 
 export default List
 
